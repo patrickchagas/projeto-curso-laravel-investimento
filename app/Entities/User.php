1796c1b119eeb9e25d75da4da4f9b8310161d40f
Patrick Chagas <patrickchagas21@gmail.com>
2019-02-17 00:38:28 -0300

    Usando Mutator para formatar dados

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -21,6 +21,36 @@ class User extends Authenticatable
     public function setPasswordAttribute($value)
 	{
 		$this->attributes['password'] = env('PASSWORD_HASH') ? bcrypt($value) : $value;
-	}
+    }
+    
+    //Setar formatação do cpf
+    public function getCpfAttribute()
+    {   
+        $cpf = $this->attributes['cpf'];    
+        
+        return substr($cpf, 0, 3) . '.' .substr($cpf, 3, 3) . '.' . substr($cpf, 7, 3) . '-' . substr($cpf, -2) ;
+    }
+
+    public function getPhoneAttribute()
+    {
+
+        $phone = $this->attributes['phone'];    
+
+        return "(" . substr($phone, 0 , 2) .") " . substr($phone, 3, 4) . "-" . substr($phone, -4); 
+
+    }
+
+    public function getBirthAttribute()
+    {
+
+        $birth = explode('-', $this->attributes['birth']);
+        if(count($birth) != 3)
+            return "";
+
+            $birth = $birth['2'] . '/' . $birth['1'] . '/' . $birth['0'];
+            return $birth;   
+
+
+    }
 
 }
